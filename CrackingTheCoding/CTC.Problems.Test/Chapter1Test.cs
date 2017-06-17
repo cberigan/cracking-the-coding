7eea9b0a796386d480a2b3c079c562f1d0c43340
Corey Berigan <cberigan1@gmail.com>
2017-06-17 15:31:29 -0400

    initial project with some solutions

---
+++ CrackingTheCoding/CTC.Problems.Test/Chapter1Test.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTC.Problems.Test
+{
+    [TestFixture]
+    public class Chapter1Test
+    {
+
+        #region 1.1 IsUniqueTest
+        [Test]
+        public void IsUniqueTestErrorChecks()
+        {
+            Assert.False(Chapter1.IsUnique(null));
+            Assert.False(Chapter1.IsUnique(""));
+            Assert.True(Chapter1.IsUnique("a"));
+            Assert.True(Chapter1.IsUnique("-"));
+        }
+
+        [Test]
+        public void IsUniqueTestFail()
+        {
+            Assert.False(Chapter1.IsUnique("qwertyuiopasdfghjkl;xcvbnm,q"));
+            Assert.False(Chapter1.IsUnique("qwertyuiopasdfghjkl;xcvbnm,q"));
+
+        }
+
+        [Test]
+        public void IsUniqueTestPass()
+        {
+            Assert.True(Chapter1.IsUnique("qwertyuiopasdfghjkl;xcvbnm,"));
+            Assert.True(Chapter1.IsUnique("1qazxcdfr567yhnm,ko90"));
+        }
+
+        #endregion
+
+        #region 1.2 Check Permutation
+        [Test]
+        public void CheckPermTest()
+        {
+            //check errors
+            Assert.False(Chapter1.CheckPermutation("abca", null));
+            Assert.False(Chapter1.CheckPermutation(null, "abc"));
+
+
+            //valid permutations
+            Assert.True(Chapter1.CheckPermutation("a", "a"));
+            Assert.True(Chapter1.CheckPermutation("asdfghjkl", "lkhjdgfsa"));
+            Assert.True(Chapter1.CheckPermutation("10293847561029384756", "09876543211234567890"));
+
+            //invalid permutations
+            Assert.False(Chapter1.CheckPermutation("alfkjlfk", "aldfkjdlf"));
+            Assert.False(Chapter1.CheckPermutation("abca", "abc"));
+            Assert.False(Chapter1.CheckPermutation("aba", "abc"));
+        }
+
+        #endregion
+
+    }
+}
---
+++ CrackingTheCoding/CTC.Problems/Chapter1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTC.Problems
+{
+    public class Chapter1
+    {
+        /// <summary>
+        /// Algorithm that determines if a string has all unique characters.
+        /// 
+        /// Time: O(n)
+        /// Space: O(1)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsUnique(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            if (s.Length == 1) return true;
+
+            int[] counts = new int[128];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (counts[s[i]] > 0) return false;
+                else counts[s[i]]++;
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// Algorithm to check if string is permutation of another string. 
+        /// The function will apply a positive count for a and a negative count for b in the counts array.
+        /// The counts array is indexed by ascii value. Every permutation should have a zerod array. 
+        /// 
+        /// Time: O(n)
+        /// Space: O(1)
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool CheckPermutation(string a, string b)
+        {
+            if (string.IsNullOrEmpty(a) || 
+                string.IsNullOrEmpty(b) ||
+                a.Length != b.Length)
+                return false;
+
+            int[] counts = new int[128];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                counts[a[i]]++;
+                counts[b[i]]--;
+            }
+            for (int i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] != 0) return false;
+            }
+            return true;
+        }
+
+
+    }
+}
