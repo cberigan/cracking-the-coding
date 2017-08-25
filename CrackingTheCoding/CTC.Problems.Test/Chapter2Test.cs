9987eae1e3fb6aa70db43794f5b8989ebe407b6b
Corey Berigan <cberigan1@gmail.com>
2017-08-24 23:19:24 -0500

    changes

--- CrackingTheCoding/CTC.Problems.Test/Chapter2Test.cs
+++ CrackingTheCoding/CTC.Problems.Test/Chapter2Test.cs
@@ -30,6 +30,47 @@ namespace CTC.Problems.Test
             CollectionAssert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void FindKthToLastTest()
+        {
+            var inputList = new List<int>() { 1, 2, 3, 1, 4, 5, 2, 6, 10, 3 };
+            var input = ListToLinked(inputList);
+            Assert.AreEqual(6, Chapter2.FindKthToLast(input, 3).Value);
+            Assert.AreEqual(4, Chapter2.FindKthToLast(input, 6).Value);
+            Assert.AreEqual(1, Chapter2.FindKthToLast(input, 10).Value);
+            Assert.IsNull(Chapter2.FindKthToLast(input, 11));
+
+        }
+
+        [Test]
+        public void RemoveMiddleNodeTest()
+        {
+            var inputList = new List<int>() { 1, 2, 3, 1, 4, 5, 2, 6, 10, 3 };
+            var expected = new List<int>() { 1, 2, 3, 1, 5, 2, 6, 10, 3 };
+            var input = ListToLinked(inputList);
+            var count = 0;
+            var middle = input;
+            while (count < 4)
+            {
+                middle = middle.Next;
+                count++;
+            }
+
+            Chapter2.RemoveMiddleNode(middle);
+
+            CollectionAssert.AreEqual(expected,LinkedToList(input));
+        }
+
+        [Test]
+        public void PartitionTest()
+        {
+            var inputList = new List<int>() { 3, 5, 8, 5, 10, 2, 1 };
+            var expected = new List<int>() { 3, 2, 1, 5, 10, 5, 8 };
+            var input = ListToLinked(inputList);
+            
+            CollectionAssert.AreEqual(expected, LinkedToList(Chapter2.Partition(input, 5)));
+        }
+
 
         public List<int> LinkedToList(SinglyNode node)
         {
@@ -54,5 +95,74 @@ namespace CTC.Problems.Test
             }
             return head;
         }
+
+        [Test]
+        public void TraverseReverseTest()
+        {
+            var inputList = new List<int>() {1,2,3,4,5,6,7,8,9};
+            var expected = new List<int>() { 9,8,7,6,5,4,3,2,1};
+            var input = ListToLinked(inputList);
+            var actual = Chapter2.TraverseReverse(input);
+            CollectionAssert.AreEqual(expected,LinkedToList(actual));
+        }
+
+        [Test]
+        public void SumsBackwardsTest()
+        {
+            var in1 = new List<int>() { 3, 5, 8 };
+            var in2 = new List<int>() { 9, 9, 9 };
+            var expected = new List<int>() { 2, 5, 8, 1 };
+            var input = ListToLinked(in1);
+            var input2 = ListToLinked(in2);
+            var actual = Chapter2.SumsBackward(input, input2);
+            CollectionAssert.AreEqual(expected, LinkedToList(actual));
+
+            in1 = new List<int>() { 3, 5 };
+            in2 = new List<int>() { 9, 9, 9 };
+            expected = new List<int>() { 2, 5, 0, 1 };
+            input = ListToLinked(in1);
+            input2 = ListToLinked(in2);
+            actual = Chapter2.SumsBackward(input, input2);
+            CollectionAssert.AreEqual(expected, LinkedToList(actual));
+        }
+
+        [Test]
+        public void SumsForwardsTest()
+        {
+            var in1 = new List<int>() {8, 5, 3};
+            var in2 = new List<int>() {9, 9, 9};
+            var expected = new List<int>() {1, 8, 5, 2};
+            var input = ListToLinked(in1);
+            var input2 = ListToLinked(in2);
+            var actual = Chapter2.SumsForward(input, input2);
+            CollectionAssert.AreEqual(expected, LinkedToList(actual));
+
+            in1 = new List<int>() {5, 3};
+            in2 = new List<int>() {9, 9, 9};
+            expected = new List<int>() {1, 0, 5, 2};
+            input = ListToLinked(in1);
+            input2 = ListToLinked(in2);
+            actual = Chapter2.SumsForward(input, input2);
+            CollectionAssert.AreEqual(expected, LinkedToList(actual));
+
+        }
+
+        [Test]
+        public void PalindromeTest()
+        {
+            var input = ListToLinked(new List<int>() { 1,1,2,2 });
+            Assert.AreEqual(true, Chapter2.Palindrome(input));
+            input = ListToLinked(new List<int>() { 1});
+            Assert.AreEqual(true, Chapter2.Palindrome(input));
+            input = ListToLinked(new List<int>() { 1, 2, 3, 4, 5, 4, 3, 2, 1 });
+            Assert.AreEqual(true, Chapter2.Palindrome(input));
+            Assert.AreEqual(true, Chapter2.Palindrome(null));
+            input = ListToLinked(new List<int>() { 1, 1, 2, 1 });
+            Assert.AreEqual(false, Chapter2.Palindrome(input));
+            input = ListToLinked(new List<int>() { 1, 2, 3, 4, 5, 4, 5, 2, 1 });
+            Assert.AreEqual(false, Chapter2.Palindrome(input));
+
+        }
+
     }
 }
--- CrackingTheCoding/CTC.Problems/Chapter2.cs
+++ CrackingTheCoding/CTC.Problems/Chapter2.cs
@@ -17,8 +17,60 @@ namespace CTC.Problems
         }
     }
 
+    public class LinkedListSingly
+    {
+        public SinglyNode Head { get; private set; }
+
+        public LinkedListSingly(SinglyNode head)
+        {
+            this.Head = head;
+        }
+
+        public void Add(int value)
+        {
+            var newNode = new SinglyNode(value);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                var node = Head;
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                }
+
+                node.Next = newNode;
+            }
+        }
+
+        public void Remove(int val)
+        {
+            if (Head == null) return;
+
+            if (Head.Value == val)
+                Head = Head.Next;
+            else
+            {
+                var node = Head;
+                while (node.Next != null)
+                {
+                    if (node.Next.Value == val)
+                    {
+                        node.Next = node.Next.Next;
+                        break;
+                    }
+                    node = node.Next;
+                }
+            }
+        }
+    }
+
     public class Chapter2
     {
+   
+
         /// <summary>
         /// Hashset version to remove dups from singly linked list.
         /// Time: O(n)
@@ -68,5 +120,241 @@ namespace CTC.Problems
                 node = node.Next;
             }
         }
+
+
+        /// <summary>
+        /// 
+        /// Time: O(n)
+        /// Space: O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static SinglyNode FindKthToLast(SinglyNode head, int k)
+        {
+            if (head == null) return null;
+            if (k < 1) return null;
+            var space = 0;
+            var curr = head;
+            var run = head;
+            while (space < k)
+            {
+                if (run == null) return null;
+                run = run.Next;
+                space++;
+            }
+
+            while (run != null)
+            {
+                run = run.Next;
+                curr = curr.Next;
+            }
+
+            return curr;
+        }
+
+        public static void RemoveMiddleNode(SinglyNode node)
+        {
+            if (node?.Next == null) return;
+
+            node.Value = node.Next.Value;
+            node.Next = node.Next.Next;
+        }
+
+
+        /// <summary>
+        /// Time: O(n)
+        /// Space: O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="partition"></param>
+        /// <returns></returns>
+        public static SinglyNode Partition(SinglyNode head, int partition)
+        {
+            if (head == null) return null;
+            var left = head;
+            var right = head.Next;
+            while (left != null && right != null)
+            {
+                if (left.Value < partition)
+                {
+                    left = left.Next;
+                    right = left.Next;
+                    continue;
+                }
+
+                if (right.Value >= partition)
+                {
+                    right = right.Next;
+                    continue;
+                }
+
+                var temp = left.Value;
+                left.Value = right.Value;
+                right.Value = temp;
+            }
+
+            return head;
+        }
+
+
+        /// <summary>
+        /// Time: O(n)
+        /// SPace: O(1)
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public static SinglyNode SumsBackward(SinglyNode l1, SinglyNode l2)
+        {
+            var head = new SinglyNode(0);
+            var node = head;
+            var carry = 0;
+            while (l1 != null || l2 != null)
+            {
+                var sum = 0;
+                if (l1 != null)
+                {
+                    sum = l1.Value;
+                    l1 = l1.Next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.Value;
+                    l2 = l2.Next;
+                }
+
+                sum += carry;
+                carry = 0;
+
+                if (sum > 9)
+                {
+                    carry = 1;
+                    sum -= 10;
+                }
+
+                node.Next = new SinglyNode(sum);
+                node = node.Next;
+            }
+            if (carry == 1)
+            {
+                node.Next = new SinglyNode(1);
+            }
+            return head.Next; // true head
+        }
+
+        /// <summary>
+        /// Time: O(n)
+        /// SPace O(1)
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public static SinglyNode SumsForward(SinglyNode l1, SinglyNode l2)
+        {
+            //traverse and reverse
+            l1 = TraverseReverse(l1);
+            l2 = TraverseReverse(l2);
+            var list = SumsBackward(l1, l2);
+            return TraverseReverse(list);
+
+        }
+
+        /// <summary>
+        /// Time: O(n)
+        /// Space: O(n)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static bool Palindrome(SinglyNode head)
+        {
+            if (head == null) return true;
+            var stack = new Stack<int>();
+            //get count
+            var count = CountLinkedList(head);
+            var middle = (int) Math.Floor((double)count / 2);
+            //travel to middle adding values to stack
+
+            var node = head;
+            var index = 0;
+            while (index <= middle)
+            {
+                index++;
+                stack.Push(node.Value);
+                node = node.Next;
+            }
+            //pop middle if odd count. doesnt matter
+            if (count % 2 == 1) stack.Pop();
+
+            while (node != null && stack.Count > 0)
+            {
+                //traverse along rght side of list while comparing top of stack
+                var compare = stack.Pop();
+                if (node.Value != compare) return false;
+                node = node.Next;
+            }
+            return true;
+
+        }
+
+        public static int CountLinkedList(SinglyNode head)
+        {
+            var count = 0;
+            var node = head;
+            while (node != null)
+            {
+                count++;
+                node = node.Next;
+            }
+            return count;
+        }
+        /// <summary>
+        /// 
+        /// node -> next -> next/next -> next^3
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static SinglyNode TraverseReverse(SinglyNode head)
+        {
+            var node = head;
+            var next = head.Next;
+
+            while (next != null)
+            {
+                var tempNext = next.Next;
+                next.Next = node;
+                node = next;
+                next = tempNext;
+            }
+            head.Next = null;
+            return node;
+        }
+
+
+        public static SinglyNode Intersection(SinglyNode l1, SinglyNode l2)
+        {
+            if (l1 == null || l2 == null) return null;
+
+            var lookup = new HashSet<SinglyNode>();
+
+            var node = l1;
+            while (node != null)
+            {
+                lookup.Add(node);
+                node = node.Next;
+            }
+
+            node = l2;
+            while (node != null)
+            {
+                if (lookup.Contains(node))
+                    return node;
+
+            }
+            return null;
+        }
+
+
+
     }
 }
